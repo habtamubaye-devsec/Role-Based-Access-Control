3a2b94170613cce1674eab1f6c7ffe5f15eca563
habtamubaye-devsec <habtamubaye2127gmail.com>
2024-10-11 07:00:00 +0000

    Update roleMiddleware.js

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -13,3 +13,4 @@ module.exports =authorizeRoles;// Auto edit at 2025-11-26T17:47:27.150Z
 // Auto edit at 2025-11-26T17:51:02.105Z
 // Auto edit at 2025-11-26T17:56:32.313Z
 // Auto edit at 2025-11-26T17:56:33.018Z
+// Auto edit at 2025-11-26T18:05:34.058Z
