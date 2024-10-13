da98dc0523121f44e9bd5bb73f9834b5b39097aa
habtamubaye-devsec <habtamubaye2127gmail.com>
2024-10-13 07:00:00 +0000

    Update dbConnect.js — auto commit

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -12,3 +12,4 @@ const dbConnect = async () => {
 
 module.exports = dbConnect;// Auto edit at 2025-11-26T17:47:27.596Z
 // Auto edit at 2025-11-26T17:47:28.734Z
+// Auto edit at 2025-11-26T17:51:00.390Z
