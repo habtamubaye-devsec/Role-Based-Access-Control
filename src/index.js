d677e335487efb708d11a02d4d3ae9b0ca433a17
habtamubaye-devsec <habtamubaye2127gmail.com>
2025-11-26 21:14:12 +0300

    Remove ffunctionality and related comments from index.js and package.json

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ const dbConnect = require('./config/dbConnect.js');
 const authRoutes = require("./routes/authRoutes.js"); 
 const userRoutes = require("./routes/userRoutes.js");
 
-// If you want auto-commit with simple-git in CommonJS:
-const { runAutoCommits } = require('../autoCommit/autoCommit.js');
+
 
 dbConnect();
 
@@ -16,21 +15,9 @@ app.use(express.json());
 app.use("/api/auth/", authRoutes);
 app.use("/api/user/", userRoutes);
 
-// Optional: trigger auto-commit at server start
-// runAutoCommits();
 
-// Optional: API to trigger auto-commit manually
-app.get("/auto-commit", async (req, res) => {
-  await runAutoCommits();
-  res.send("Auto commits executed and pushed to GitHub (main)");
-});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-// Auto edit at 2025-11-26T17:51:03.462Z
-// Auto edit at 2025-11-26T17:51:03.869Z
-// Auto edit at 2025-11-26T17:51:04.408Z
-// Auto edit at 2025-11-26T17:56:35.723Z
-// Auto edit at 2025-11-26T18:00:30.775Z
